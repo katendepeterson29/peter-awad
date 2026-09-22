cf4419fe1d00c4f9a57a470452edeba3f79279df
katendepeterson29 <katendepeterson6@gmail.com>
2026-09-22 16:27:35 +0300

    fixed route, controllers and db migrations

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function contact()
+    {
+        return view('contact');
+    }
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $contact = new Contact();
+        $contact->full_name = $request->input('full_name');
+        $contact->campus_email = $request->input('campus_email');
+        $contact->department = $request->input('department');
+        $contact->message = $request->input('message');
+        $contact->save();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
